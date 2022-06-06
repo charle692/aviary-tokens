6f67268df9a8e647bc024b92ceca5bad880f0168
Alexandra Lim <alexandra.lim@fullscript.com>
2022-06-06 16:30:51 -0400

    update source in config.json and rebuild

---
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,69 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly
+// Generated on Mon, 06 Jun 2022 20:16:03 GMT
+
+
+import UIKit
+
+public enum StyleDictionaryEnum {
+    public static let globalTokensBlue100 = #F5FAFF
+    public static let globalTokensBlue200 = #E6F1FC
+    public static let globalTokensBlue300 = #C0D8F0
+    public static let globalTokensBlue400 = #88B1D9
+    public static let globalTokensBlue500 = #5D96CF
+    public static let globalTokensBlue600 = #3971A8
+    public static let globalTokensBlue700 = #21588F
+    public static let globalTokensBlue800 = #194673
+    public static let globalTokensBody = [object Object]
+    public static let globalTokensCaption = [object Object]
+    public static let globalTokensFootnote = [object Object]
+    public static let globalTokensGreen100 = #FAFFFC
+    public static let globalTokensGreen200 = #EBF2EF
+    public static let globalTokensGreen300 = #D1E0D9
+    public static let globalTokensGreen400 = #B6CFC2
+    public static let globalTokensGreen500 = #86B09B
+    public static let globalTokensGreen600 = #307553
+    public static let globalTokensGreen700 = #275E43
+    public static let globalTokensGreen800 = #244C38
+    public static let globalTokensGrey100 = #FCFEFF
+    public static let globalTokensGrey200 = #F5F7FA
+    public static let globalTokensGrey300 = #E6EDF5
+    public static let globalTokensGrey400 = #C8D3E0
+    public static let globalTokensGrey500 = #596D84
+    public static let globalTokensGrey600 = #475A70
+    public static let globalTokensGrey700 = #36485C
+    public static let globalTokensGrey800 = #2E3A47
+    public static let globalTokensH1 = [object Object]
+    public static let globalTokensH2 = [object Object]
+    public static let globalTokensH3 = [object Object]
+    public static let globalTokensH4 = [object Object]
+    public static let globalTokensH5 = [object Object]
+    public static let globalTokensOrange100 = #FFFAF5
+    public static let globalTokensOrange200 = #FFF3E8
+    public static let globalTokensOrange300 = #F3D8C0
+    public static let globalTokensOrange400 = #E7B88F
+    public static let globalTokensOrange500 = #CF8545
+    public static let globalTokensOrange600 = #B4631D
+    public static let globalTokensOrange700 = #8E4D14
+    public static let globalTokensOrange800 = #784213
+    public static let globalTokensPurple100 = #FCFAFF
+    public static let globalTokensPurple200 = #F2ECFE
+    public static let globalTokensPurple300 = #D7CEE9
+    public static let globalTokensPurple400 = #B9ABD5
+    public static let globalTokensPurple500 = #8471AB
+    public static let globalTokensPurple600 = #533E7D
+    public static let globalTokensPurple700 = #3B2566
+    public static let globalTokensPurple800 = #2A174F
+    public static let globalTokensRed100 = #FFF7F9
+    public static let globalTokensRed200 = #FEEEF2
+    public static let globalTokensRed300 = #F0C4CD
+    public static let globalTokensRed400 = #E296A6
+    public static let globalTokensRed500 = #C6516A
+    public static let globalTokensRed600 = #AF2645
+    public static let globalTokensRed700 = #980B29
+    public static let globalTokensRed800 = #800D25
+}
---
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,69 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly
+// Generated on Mon, 06 Jun 2022 20:16:03 GMT
+
+
+import UIKit
+
+internal struct StyleDictionaryStruct {
+    internal static let globalTokensBlue100 = #F5FAFF
+    internal static let globalTokensBlue200 = #E6F1FC
+    internal static let globalTokensBlue300 = #C0D8F0
+    internal static let globalTokensBlue400 = #88B1D9
+    internal static let globalTokensBlue500 = #5D96CF
+    internal static let globalTokensBlue600 = #3971A8
+    internal static let globalTokensBlue700 = #21588F
+    internal static let globalTokensBlue800 = #194673
+    internal static let globalTokensBody = [object Object]
+    internal static let globalTokensCaption = [object Object]
+    internal static let globalTokensFootnote = [object Object]
+    internal static let globalTokensGreen100 = #FAFFFC
+    internal static let globalTokensGreen200 = #EBF2EF
+    internal static let globalTokensGreen300 = #D1E0D9
+    internal static let globalTokensGreen400 = #B6CFC2
+    internal static let globalTokensGreen500 = #86B09B
+    internal static let globalTokensGreen600 = #307553
+    internal static let globalTokensGreen700 = #275E43
+    internal static let globalTokensGreen800 = #244C38
+    internal static let globalTokensGrey100 = #FCFEFF
+    internal static let globalTokensGrey200 = #F5F7FA
+    internal static let globalTokensGrey300 = #E6EDF5
+    internal static let globalTokensGrey400 = #C8D3E0
+    internal static let globalTokensGrey500 = #596D84
+    internal static let globalTokensGrey600 = #475A70
+    internal static let globalTokensGrey700 = #36485C
+    internal static let globalTokensGrey800 = #2E3A47
+    internal static let globalTokensH1 = [object Object]
+    internal static let globalTokensH2 = [object Object]
+    internal static let globalTokensH3 = [object Object]
+    internal static let globalTokensH4 = [object Object]
+    internal static let globalTokensH5 = [object Object]
+    internal static let globalTokensOrange100 = #FFFAF5
+    internal static let globalTokensOrange200 = #FFF3E8
+    internal static let globalTokensOrange300 = #F3D8C0
+    internal static let globalTokensOrange400 = #E7B88F
+    internal static let globalTokensOrange500 = #CF8545
+    internal static let globalTokensOrange600 = #B4631D
+    internal static let globalTokensOrange700 = #8E4D14
+    internal static let globalTokensOrange800 = #784213
+    internal static let globalTokensPurple100 = #FCFAFF
+    internal static let globalTokensPurple200 = #F2ECFE
+    internal static let globalTokensPurple300 = #D7CEE9
+    internal static let globalTokensPurple400 = #B9ABD5
+    internal static let globalTokensPurple500 = #8471AB
+    internal static let globalTokensPurple600 = #533E7D
+    internal static let globalTokensPurple700 = #3B2566
+    internal static let globalTokensPurple800 = #2A174F
+    internal static let globalTokensRed100 = #FFF7F9
+    internal static let globalTokensRed200 = #FEEEF2
+    internal static let globalTokensRed300 = #F0C4CD
+    internal static let globalTokensRed400 = #E296A6
+    internal static let globalTokensRed500 = #C6516A
+    internal static let globalTokensRed600 = #AF2645
+    internal static let globalTokensRed700 = #980B29
+    internal static let globalTokensRed800 = #800D25
+}
