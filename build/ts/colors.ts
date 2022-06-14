1a3eb8edcb8d38b03c7a56910aca29307deef625
Catherine Aylward <catherine.aylward@fullscript.com>
2022-06-14 13:15:49 -0400

    wip

---
+++ build/ts/colors.ts
@@ -0,0 +1,54 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 14 Jun 2022 15:30:49 GMT
+ */
+
+export const GlobalColorsGreen100 = "#FAFFFC";
+export const GlobalColorsGreen200 = "#EBF2EF";
+export const GlobalColorsGreen300 = "#D1E0D9";
+export const GlobalColorsGreen400 = "#B6CFC2";
+export const GlobalColorsGreen500 = "#86B09B";
+export const GlobalColorsGreen600 = "#307553";
+export const GlobalColorsGreen700 = "#275E43";
+export const GlobalColorsGreen800 = "#244C38";
+export const GlobalColorsGreen900 = "#FFF";
+export const GlobalColorsBlue100 = "#F5FAFF";
+export const GlobalColorsBlue200 = "#E6F1FC";
+export const GlobalColorsBlue300 = "#C0D8F0";
+export const GlobalColorsBlue400 = "#88B1D9";
+export const GlobalColorsBlue500 = "#5D96CF";
+export const GlobalColorsBlue600 = "#3971A8";
+export const GlobalColorsBlue700 = "#21588F";
+export const GlobalColorsBlue800 = "#194673";
+export const GlobalColorsRed100 = "#FFF7F9";
+export const GlobalColorsRed200 = "#FEEEF2";
+export const GlobalColorsRed300 = "#F0C4CD";
+export const GlobalColorsRed400 = "#E296A6";
+export const GlobalColorsRed500 = "#C6516A";
+export const GlobalColorsRed600 = "#AF2645";
+export const GlobalColorsRed700 = "#980B29";
+export const GlobalColorsRed800 = "#800D25";
+export const GlobalColorsPurple100 = "#FCFAFF";
+export const GlobalColorsPurple200 = "#F2ECFE";
+export const GlobalColorsPurple300 = "#D7CEE9";
+export const GlobalColorsPurple400 = "#B9ABD5";
+export const GlobalColorsPurple500 = "#8471AB";
+export const GlobalColorsPurple600 = "#533E7D";
+export const GlobalColorsPurple700 = "#3B2566";
+export const GlobalColorsPurple800 = "#2A174F";
+export const GlobalColorsOrange100 = "#FFFAF5";
+export const GlobalColorsOrange200 = "#FFF3E8";
+export const GlobalColorsOrange300 = "#F3D8C0";
+export const GlobalColorsOrange400 = "#E7B88F";
+export const GlobalColorsOrange500 = "#CF8545";
+export const GlobalColorsOrange600 = "#B4631D";
+export const GlobalColorsOrange700 = "#8E4D14";
+export const GlobalColorsOrange800 = "#784213";
+export const GlobalColorsGrey100 = "#FCFEFF";
+export const GlobalColorsGrey200 = "#F5F7FA";
+export const GlobalColorsGrey300 = "#E6EDF5";
+export const GlobalColorsGrey400 = "#C8D3E0";
+export const GlobalColorsGrey500 = "#596D84";
+export const GlobalColorsGrey600 = "#475A70";
+export const GlobalColorsGrey700 = "#36485C";
+export const GlobalColorsGrey800 = "#2E3A47";
