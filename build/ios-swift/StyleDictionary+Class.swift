9350fccc1594f80a9d63cb8c126fd3fc06747b35
Alexandra Lim <alexandra.lim@fullscript.com>
2022-06-15 18:34:20 -0400

    new structure is working, need to figure out a workaround for filter

---
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -0,0 +1,19 @@
+
+//
+// StyleDictionary+Class.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 15 Jun 2022 18:49:38 GMT
+
+
+import UIKit
+
+public class StyleDictionaryClass {
+    public static let display2xlRegularFontFamily = Inter
+    public static let display2xlRegularFontSize = 72
+    public static let display2xlRegularFontWeight = Regular
+    public static let fontFamiliesInter = Inter
+    public static let fontSize10 = 72
+    public static let fontWeightsInter0 = Regular
+}
--- build/ts/typography.ts
+++ build/ts/typography.ts
@@ -1,13 +1,11 @@
 /**
  * Do not edit directly
- * Generated on Wed, 15 Jun 2022 15:44:11 GMT
+ * Generated on Wed, 15 Jun 2022 22:33:39 GMT
  */
 
-export const GlobalTypographyH1 = {"fontFamily":"Mulish","fontWeight":"700","lineHeight":"40","fontSize":"40","letterSpacing":"0%","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
-export const GlobalTypographyH2 = {"fontFamily":"Mulish","fontWeight":"700","lineHeight":"40","fontSize":"32","letterSpacing":"0%","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
-export const GlobalTypographyH3 = {"fontFamily":"Mulish","fontWeight":"700","lineHeight":"32","fontSize":"24","letterSpacing":"0%","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
-export const GlobalTypographyH4 = {"fontFamily":"Mulish","fontWeight":"700","lineHeight":"24","fontSize":"20","letterSpacing":"0%","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
-export const GlobalTypographyH5 = {"fontFamily":"Mulish","fontWeight":"700","lineHeight":"24","fontSize":"16","letterSpacing":"0%","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
-export const GlobalTypographyBody = {"fontFamily":"Mulish","fontWeight":"500","lineHeight":"24","fontSize":"16","letterSpacing":"0%","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
-export const GlobalTypographyFootnote = {"fontFamily":"Mulish","fontWeight":"500","lineHeight":"16","fontSize":"14","letterSpacing":"0%","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
-export const GlobalTypographyCaption = {"fontFamily":"Mulish","fontWeight":"500","lineHeight":"16","fontSize":"12","letterSpacing":"0%","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
+export const GlobalTypographyH3 = {"fontFamily":"Mulish","fontWeight":"700","lineHeight":"32","fontSize":"24","letterSpacing":"0","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
+export const GlobalTypographyH4 = {"fontFamily":"Mulish","fontWeight":"700","lineHeight":"24","fontSize":"20","letterSpacing":"0","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
+export const GlobalTypographyH5 = {"fontFamily":"Mulish","fontWeight":"700","lineHeight":"24","fontSize":"16","letterSpacing":"0","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
+export const GlobalTypographyBody = {"fontFamily":"Mulish","fontWeight":"500","lineHeight":"24","fontSize":"16","letterSpacing":"0","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
+export const GlobalTypographyFootnote = {"fontFamily":"Mulish","fontWeight":"500","lineHeight":"16","fontSize":"12","letterSpacing":"0","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
+export const GlobalTypographyCaption = {"fontFamily":"Mulish","fontWeight":"500","lineHeight":"16","fontSize":"14","letterSpacing":"0","paragraphSpacing":"0","textDecoration":"none","textCase":"none"};
