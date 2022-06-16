b1b205fc1f0769cfcb90b243d51cab6e3d08bdc4
Alexandra Lim <alexandra.lim@fullscript.com>
2022-06-16 12:31:53 -0400

    add custom filter for transforming tokens

---
+++ build.js
@@ -0,0 +1,51 @@
+const StyleDictionary = require("style-dictionary").extend("config.json");
+
+StyleDictionary.registerFilter({
+  name: "filter-typography",
+  matcher: function (prop) {
+    return prop.attributes.type === "typography";
+  },
+});
+
+StyleDictionary.extend({
+  source: ["tokens/**/*.json"],
+  platforms: {
+    Owlery: {
+      transformGroup: "scss",
+      buildPath: "build/scss/",
+      files: [
+        {
+          destination: "colors.scss",
+          format: "scss/variables",
+          filter: {
+            type: "color",
+          },
+        },
+        {
+          destination: "typography.scss",
+          format: "scss/variables",
+          filter: "filter-typography",
+        },
+      ],
+    },
+
+    Aviary: {
+      transformGroup: "js",
+      buildPath: "build/ts/",
+      files: [
+        {
+          format: "javascript/es6",
+          destination: "colors.ts",
+          filter: {
+            type: "color",
+          },
+        },
+        {
+          format: "javascript/es6",
+          destination: "typography.ts",
+          filter: "filter-typography",
+        },
+      ],
+    },
+  },
+}).buildAllPlatforms();
