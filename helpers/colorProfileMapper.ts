3f2f5c46772a18cdc0b50463418d753fbb2e9f92
Catherine Aylward` <catherine.aylward@fullscript.com>
2022-08-24 10:47:22 -0400

    mapper obj

---
+++ helpers/colorProfileMapper.ts
@@ -0,0 +1,11 @@
+import { AviaryTheme } from "../themes";
+
+export const colorProfileMapper = (currentTheme: AviaryTheme) => {
+  return {
+    primary: currentTheme.primary,
+    info: currentTheme.info,
+    warning: currentTheme.warning,
+    danger: currentTheme.danger,
+    highlight: currentTheme.highlight,
+  };
+};
