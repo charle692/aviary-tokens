558cf07819ec094f44cdfa25891693d1dd9ba20f
alexandra-lim <alexandra-lim@users.noreply.github.com>
2022-08-24 18:21:31 +0000

    Prettified Code!

--- build/ts/themes/light.d.ts
+++ build/ts/themes/light.d.ts
@@ -3,13 +3,131 @@
  * Generated on Wed, 24 Aug 2022 18:21:08 GMT
  */
 
-export const text : {header : string,emphasis : string,body : string,subdued : string,onBackground : string};
-export const surface : {level0 : string,level1 : string,level2 : string,level3 : string,interactiveBackground : string,interactiveHover : string,interactiveActive : string};
-export const primary : {textBase : string,textHover : string,textActive : string,backgroundBase : string,backgroundHover : string,backgroundActive : string,backgroundMuted : string,backgroundMutedHover : string,backgroundMutedActive : string,backgroundBox : string,borderBase : string,borderHover : string,borderActive : string};
-export const info : {textBase : string,textHover : string,textActive : string,backgroundBase : string,backgroundHover : string,backgroundActive : string,backgroundMuted : string,backgroundMutedHover : string,backgroundMutedActive : string,backgroundBox : string,borderBase : string,borderHover : string,borderActive : string};
-export const warning : {textBase : string,textHover : string,textActive : string,backgroundBase : string,backgroundHover : string,backgroundActive : string,backgroundMuted : string,backgroundMutedHover : string,backgroundMutedActive : string,backgroundBox : string,borderBase : string,borderHover : string,borderActive : string};
-export const danger : {textBase : string,textHover : string,textActive : string,backgroundBase : string,backgroundHover : string,backgroundActive : string,backgroundMuted : string,backgroundMutedHover : string,backgroundMutedActive : string,backgroundBox : string,borderBase : string,borderHover : string,borderActive : string};
-export const highlight : {textBase : string,textHover : string,textActive : string,backgroundBase : string,backgroundHover : string,backgroundActive : string,backgroundMuted : string,backgroundMutedHover : string,backgroundMutedActive : string,backgroundBox : string,borderBase : string,borderHover : string,borderActive : string};
-export const system : {textBase : string,textHover : string,textActive : string,backgroundBase : string,backgroundMuted : string,backgroundMutedHover : string,backgroundMutedActive : string,backgroundBox : string,borderBase : string,borderHover : string,borderActive : string,borderFocused : string};
-export const disabled : {text : string,background : string,nakedContent : string,nakedBackground : string,border : string,inputBackground : string,inputLabelText : string};
-export const input : {backgroundBase : string,backgroundHover : string,backgroundInformation : string,textBase : string,textInformation : string,textInformationError : string,textLabel : string,textFloatingLabel : string,textFloatingLabelError : string,textPlaceholder : string,borderBase : string,borderError : string};
+export const text: {
+  header: string;
+  emphasis: string;
+  body: string;
+  subdued: string;
+  onBackground: string;
+};
+export const surface: {
+  level0: string;
+  level1: string;
+  level2: string;
+  level3: string;
+  interactiveBackground: string;
+  interactiveHover: string;
+  interactiveActive: string;
+};
+export const primary: {
+  textBase: string;
+  textHover: string;
+  textActive: string;
+  backgroundBase: string;
+  backgroundHover: string;
+  backgroundActive: string;
+  backgroundMuted: string;
+  backgroundMutedHover: string;
+  backgroundMutedActive: string;
+  backgroundBox: string;
+  borderBase: string;
+  borderHover: string;
+  borderActive: string;
+};
+export const info: {
+  textBase: string;
+  textHover: string;
+  textActive: string;
+  backgroundBase: string;
+  backgroundHover: string;
+  backgroundActive: string;
+  backgroundMuted: string;
+  backgroundMutedHover: string;
+  backgroundMutedActive: string;
+  backgroundBox: string;
+  borderBase: string;
+  borderHover: string;
+  borderActive: string;
+};
+export const warning: {
+  textBase: string;
+  textHover: string;
+  textActive: string;
+  backgroundBase: string;
+  backgroundHover: string;
+  backgroundActive: string;
+  backgroundMuted: string;
+  backgroundMutedHover: string;
+  backgroundMutedActive: string;
+  backgroundBox: string;
+  borderBase: string;
+  borderHover: string;
+  borderActive: string;
+};
+export const danger: {
+  textBase: string;
+  textHover: string;
+  textActive: string;
+  backgroundBase: string;
+  backgroundHover: string;
+  backgroundActive: string;
+  backgroundMuted: string;
+  backgroundMutedHover: string;
+  backgroundMutedActive: string;
+  backgroundBox: string;
+  borderBase: string;
+  borderHover: string;
+  borderActive: string;
+};
+export const highlight: {
+  textBase: string;
+  textHover: string;
+  textActive: string;
+  backgroundBase: string;
+  backgroundHover: string;
+  backgroundActive: string;
+  backgroundMuted: string;
+  backgroundMutedHover: string;
+  backgroundMutedActive: string;
+  backgroundBox: string;
+  borderBase: string;
+  borderHover: string;
+  borderActive: string;
+};
+export const system: {
+  textBase: string;
+  textHover: string;
+  textActive: string;
+  backgroundBase: string;
+  backgroundMuted: string;
+  backgroundMutedHover: string;
+  backgroundMutedActive: string;
+  backgroundBox: string;
+  borderBase: string;
+  borderHover: string;
+  borderActive: string;
+  borderFocused: string;
+};
+export const disabled: {
+  text: string;
+  background: string;
+  nakedContent: string;
+  nakedBackground: string;
+  border: string;
+  inputBackground: string;
+  inputLabelText: string;
+};
+export const input: {
+  backgroundBase: string;
+  backgroundHover: string;
+  backgroundInformation: string;
+  textBase: string;
+  textInformation: string;
+  textInformationError: string;
+  textLabel: string;
+  textFloatingLabel: string;
+  textFloatingLabelError: string;
+  textPlaceholder: string;
+  borderBase: string;
+  borderError: string;
+};
