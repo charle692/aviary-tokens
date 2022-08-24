6f130fb3528cf4059ca4637e327fc4f31fb352cb
alexandra-lim <alexandra-lim@users.noreply.github.com>
2022-08-24 18:23:04 +0000

    Built and updated design tokens

--- build/native/colors.d.ts
+++ build/native/colors.d.ts
@@ -1,66 +1,12 @@
 /**
  * Do not edit directly
- * Generated on Wed, 24 Aug 2022 18:21:08 GMT
+ * Generated on Wed, 24 Aug 2022 18:23:03 GMT
  */
 
-export const green: {
-  100: string;
-  200: string;
-  300: string;
-  400: string;
-  500: string;
-  600: string;
-  700: string;
-  800: string;
-};
-export const blue: {
-  100: string;
-  200: string;
-  300: string;
-  400: string;
-  500: string;
-  600: string;
-  700: string;
-  800: string;
-};
-export const red: {
-  100: string;
-  200: string;
-  300: string;
-  400: string;
-  500: string;
-  600: string;
-  700: string;
-  800: string;
-};
-export const purple: {
-  100: string;
-  200: string;
-  300: string;
-  400: string;
-  500: string;
-  600: string;
-  700: string;
-  800: string;
-};
-export const orange: {
-  100: string;
-  200: string;
-  300: string;
-  400: string;
-  500: string;
-  600: string;
-  700: string;
-  800: string;
-};
-export const grey: {
-  100: string;
-  200: string;
-  300: string;
-  400: string;
-  500: string;
-  600: string;
-  700: string;
-  800: string;
-};
-export const shades: { white: string; black: string; transparent: string };
+export const green : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const blue : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const red : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const purple : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const orange : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const grey : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const shades : {white : string,black : string,transparent : string};
