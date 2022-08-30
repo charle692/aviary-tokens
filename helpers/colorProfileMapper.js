4022df53c3edd7d1ff51e9e51d3f48d9c911e360
alexandra-lim <alexandra-lim@users.noreply.github.com>
2022-08-30 14:54:43 +0000

    Files compiled!

---
+++ helpers/colorProfileMapper.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.colorProfileMapper = void 0;
+const colorProfileMapper = (currentTheme) => {
+    return {
+        primary: currentTheme.primary,
+        info: currentTheme.info,
+        warning: currentTheme.warning,
+        danger: currentTheme.danger,
+        // highlight: currentTheme.highlight,
+    };
+};
+exports.colorProfileMapper = colorProfileMapper;
