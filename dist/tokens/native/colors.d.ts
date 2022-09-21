eb785b7e6523626ce353ad7099811df3af891a1d
Ian Clarke <me@iryanclarke.com>
2022-09-21 15:10:05 -0400

    fix: Overlays & Separator tokens (#84)
    * Adding in overlay tokens
    
    * Transformed tokens
    
    * Built and updated design tokens
    
    * Files compiled!
    
    * Adding separator value
    
    * Transformed tokens
    
    * Built and updated design tokens
    
    * Files compiled!
    
    Co-authored-by: iryanclarke <iryanclarke@users.noreply.github.com>

--- dist/tokens/native/colors.d.ts
+++ dist/tokens/native/colors.d.ts
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Mon, 12 Sep 2022 15:14:16 GMT
+ * Generated on Wed, 21 Sep 2022 14:00:11 GMT
  */
 
 export const green : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
--- dist/tokens/ts/colors.js
+++ dist/tokens/ts/colors.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Mon, 12 Sep 2022 15:14:16 GMT
+ * Generated on Wed, 21 Sep 2022 14:00:11 GMT
  */
 
 module.exports = {green : {100 : "#FAFFFC",200 : "#EBF2EF",300 : "#D1E0D9",400 : "#B6CFC2",500 : "#86B09B",600 : "#307553",700 : "#275E43",800 : "#244C38"},
