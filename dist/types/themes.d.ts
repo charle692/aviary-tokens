c843757d75e97271158b4d16b8f8230f18a19c7c
CCAyl <CCAyl@users.noreply.github.com>
2022-09-26 18:48:09 +0000

    Transformed tokens

--- dist/types/themes.d.ts
+++
@@ -1,5 +0,0 @@
-import * as light from "../../dist/tokens/ts/themes/light";
-declare type AviaryTheme = typeof light;
-export { light };
-export type { AviaryTheme };
-//# sourceMappingURL=themes.d.ts.map
