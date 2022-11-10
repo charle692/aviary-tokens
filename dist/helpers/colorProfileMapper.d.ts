ddb52ad49321b1a543554cddd170b2f0e0183a9f
alexandra-lim <alexandra-lim@users.noreply.github.com>
2022-11-10 15:19:32 +0000

    Files compiled!

---
+++ dist/helpers/colorProfileMapper.d.ts
@@ -0,0 +1,94 @@
+import type { AviaryTheme } from "../types/themes";
+export declare const colorProfileMapper: (currentTheme: AviaryTheme) => {
+    primary: {
+        textBase: string;
+        textHover: string;
+        textActive: string;
+        backgroundBase: string;
+        backgroundHover: string;
+        backgroundActive: string;
+        backgroundMuted: string;
+        backgroundMutedHover: string;
+        backgroundMutedActive: string;
+        backgroundBox: string;
+        borderBase: string;
+        borderHover: string;
+        borderActive: string;
+    };
+    info: {
+        textBase: string;
+        textHover: string;
+        textActive: string;
+        backgroundBase: string;
+        backgroundHover: string;
+        backgroundActive: string;
+        backgroundMuted: string;
+        backgroundMutedHover: string;
+        backgroundMutedActive: string;
+        backgroundBox: string;
+        borderBase: string;
+        borderHover: string;
+        borderActive: string;
+    };
+    warning: {
+        textBase: string;
+        textHover: string;
+        textActive: string;
+        backgroundBase: string;
+        backgroundHover: string;
+        backgroundActive: string;
+        backgroundMuted: string;
+        backgroundMutedHover: string;
+        backgroundMutedActive: string;
+        backgroundBox: string;
+        borderBase: string;
+        borderHover: string;
+        borderActive: string;
+    };
+    danger: {
+        textBase: string;
+        textHover: string;
+        textActive: string;
+        backgroundBase: string;
+        backgroundHover: string;
+        backgroundActive: string;
+        backgroundMuted: string;
+        backgroundMutedHover: string;
+        backgroundMutedActive: string;
+        backgroundBox: string;
+        borderBase: string;
+        borderHover: string;
+        borderActive: string;
+    };
+    highlight: {
+        textBase: string;
+        textHover: string;
+        textActive: string;
+        backgroundBase: string;
+        backgroundHover: string;
+        backgroundActive: string;
+        backgroundMuted: string;
+        backgroundMutedHover: string;
+        backgroundMutedActive: string;
+        backgroundBox: string;
+        borderBase: string;
+        borderHover: string;
+        borderActive: string;
+    };
+    system: {
+        textLabelEmphasis: string;
+        textBase: string;
+        textHover: string;
+        textActive: string;
+        backgroundBase: string;
+        backgroundMuted: string;
+        backgroundMutedHover: string;
+        backgroundMutedActive: string;
+        backgroundBox: string;
+        borderBase: string;
+        borderHover: string;
+        borderActive: string;
+        borderFocused: string;
+    };
+};
+//# sourceMappingURL=colorProfileMapper.d.ts.map
