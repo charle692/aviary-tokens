564f510598904cf7a665f6bd33b6fa3f46c28f32
alexandra-lim <alexandra-lim@users.noreply.github.com>
2022-11-21 22:38:44 +0000

    Built and updated design tokens

---
+++ dist/tokens/ts/colors.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 21 Nov 2022 22:38:43 GMT
+ */
+
+export const green : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const blue : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const red : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const purple : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const orange : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const grey : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const shades : {white : string,black : string,transparent : string};
