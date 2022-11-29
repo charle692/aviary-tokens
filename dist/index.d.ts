0201d765d5f0b2032da0e19997037fbce321b509
alexandra-lim <alexandra-lim@users.noreply.github.com>
2022-11-29 16:31:25 +0000

    Files compiled!

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+export * from "./helpers";
+export * from "./types";
+export * as typography from "../dist/tokens/ts/typography";
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/types/themes.d.ts
@@ -0,0 +1,5 @@
+import * as light from "../../dist/tokens/ts/themes/light";
+declare type AviaryTheme = typeof light;
+export { light };
+export type { AviaryTheme };
+//# sourceMappingURL=themes.d.ts.map
