b24a2a1ce1863cab5055605afb030887771bafe1
Catherine Aylward` <catherine.aylward@fullscript.com>
2023-02-01 12:24:48 -0500

    rm

--- themes.d.ts
+++ themes.d.ts
@@ -2,19 +2,7 @@ import * as light from "./dist/tokens/ts/themes/light";
 import * as dark from "./dist/tokens/ts/themes/dark";
 import * as emerson from "./dist/tokens/ts/themes/emerson";
 
-import * as coreDarkDocumentation from "./dist/documentation/core-dark-colors";
-import * as coreLightDocumentation from "./dist/documentation/core-light-colors";
-import * as lightDocumentation from "./dist/documentation/themes/light";
-import * as darkDocumentation from "./dist/documentation/themes/dark";
 declare type AviaryTheme = typeof light;
-export {
-  dark,
-  light,
-  emerson,
-  coreDarkDocumentation,
-  coreLightDocumentation,
-  lightDocumentation,
-  darkDocumentation,
-};
+export { dark, light, emerson };
 export type { AviaryTheme };
 //# sourceMappingURL=themes.d.ts.map
