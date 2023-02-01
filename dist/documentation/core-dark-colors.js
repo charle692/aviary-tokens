d891c465d931e3f104db9f3cfc302c5a3817be7f
CCAyl <CCAyl@users.noreply.github.com>
2023-02-01 16:13:52 +0000

    Built and updated design tokens

---
+++ dist/documentation/core-dark-colors.js
@@ -0,0 +1,216 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 01 Feb 2023 16:13:51 GMT
+ */
+
+module.exports = {green : {100:{
+            name: "green.100",
+            hex: "#0C291E",
+            
+          },200:{
+            name: "green.200",
+            hex: "#13402F",
+            
+          },300:{
+            name: "green.300",
+            hex: "#1A5740",
+            
+          },400:{
+            name: "green.400",
+            hex: "#256E53",
+            
+          },500:{
+            name: "green.500",
+            hex: "#288563",
+            
+          },600:{
+            name: "green.600",
+            hex: "#42AD86",
+            
+          },700:{
+            name: "green.700",
+            hex: "#86D6B9",
+            
+          },800:{
+            name: "green.800",
+            hex: "#DBFFF1",
+            
+          }},
+blue : {100:{
+            name: "blue.100",
+            hex: "#0C1B29",
+            
+          },200:{
+            name: "blue.200",
+            hex: "#142C45",
+            
+          },300:{
+            name: "blue.300",
+            hex: "#1F4366",
+            
+          },400:{
+            name: "blue.400",
+            hex: "#285685",
+            
+          },500:{
+            name: "blue.500",
+            hex: "#4577AD",
+            
+          },600:{
+            name: "blue.600",
+            hex: "#6597CE",
+            
+          },700:{
+            name: "blue.700",
+            hex: "#96BDE7",
+            
+          },800:{
+            name: "blue.800",
+            hex: "#D1E8FF",
+            
+          }},
+red : {100:{
+            name: "red.100",
+            hex: "#290F16",
+            
+          },200:{
+            name: "red.200",
+            hex: "#451522",
+            
+          },300:{
+            name: "red.300",
+            hex: "#611D30",
+            
+          },400:{
+            name: "red.400",
+            hex: "#8F2843",
+            
+          },500:{
+            name: "red.500",
+            hex: "#B23655",
+            
+          },600:{
+            name: "red.600",
+            hex: "#C74C6B",
+            
+          },700:{
+            name: "red.700",
+            hex: "#EA8AA3",
+            
+          },800:{
+            name: "red.800",
+            hex: "#FFD1DD",
+            
+          }},
+purple : {100:{
+            name: "purple.100",
+            hex: "#200F29",
+            
+          },200:{
+            name: "purple.200",
+            hex: "#33193F",
+            
+          },300:{
+            name: "purple.300",
+            hex: "#432652",
+            
+          },400:{
+            name: "purple.400",
+            hex: "#542F66",
+            
+          },500:{
+            name: "purple.500",
+            hex: "#693B80",
+            
+          },600:{
+            name: "purple.600",
+            hex: "#865C99",
+            
+          },700:{
+            name: "purple.700",
+            hex: "#B990CC",
+            
+          },800:{
+            name: "purple.800",
+            hex: "#F0F1FF",
+            
+          }},
+orange : {100:{
+            name: "orange.100",
+            hex: "#29170C",
+            
+          },200:{
+            name: "orange.200",
+            hex: "#4A2915",
+            
+          },300:{
+            name: "orange.300",
+            hex: "#663A1E",
+            
+          },400:{
+            name: "orange.400",
+            hex: "#8A4E29",
+            
+          },500:{
+            name: "orange.500",
+            hex: "#AD6234",
+            
+          },600:{
+            name: "orange.600",
+            hex: "#CF7945",
+            
+          },700:{
+            name: "orange.700",
+            hex: "#E7AA84",
+            
+          },800:{
+            name: "orange.800",
+            hex: "#FFE3D1",
+            
+          }},
+grey : {100:{
+            name: "grey.100",
+            hex: "#101012",
+            
+          },200:{
+            name: "grey.200",
+            hex: "#1A1D21",
+            
+          },300:{
+            name: "grey.300",
+            hex: "#22272B",
+            
+          },400:{
+            name: "grey.400",
+            hex: "#2D3238",
+            
+          },500:{
+            name: "grey.500",
+            hex: "#515357",
+            
+          },600:{
+            name: "grey.600",
+            hex: "#616568",
+            
+          },700:{
+            name: "grey.700",
+            hex: "#98999B",
+            
+          },800:{
+            name: "grey.800",
+            hex: "#E8E9EA",
+            
+          }},
+shades : {white:{
+            name: "shades.white",
+            hex: "#FFFFFF",
+            
+          },black:{
+            name: "shades.black",
+            hex: "#000000",
+            
+          },transparent:{
+            name: "shades.transparent",
+            hex: "transparent",
+            
+          }},};
