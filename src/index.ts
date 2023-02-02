d6243f80598246c32a32bd6417d54c7442ae5001
Catherine Aylward` <catherine.aylward@fullscript.com>
2023-02-02 12:14:38 -0500

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+// add all exports here!
 export * from "./helpers";
-export * from "./types"
-export * as typography from "../dist/tokens/ts/typography"; 
+export * from "./types";
+export * as typography from "../dist/tokens/ts/typography";
+export * as lightThemeDocumentation from "../dist/documentation/themes/light";
+export * as darkThemeDocumentation from "../dist/documentation/themes/dark";
+
+// if we ever want to create pallete tiles in docusuarus
+export * as coreDarkDocumentation from "../dist/documentation/core-dark-colors";
+export * as coreLightDocumentation from "../dist/documentation/core-light-colors";
