e3d525405671219e9a6172004dbc2cec512d671a
alexandra-lim <alexandra-lim@users.noreply.github.com>
2023-02-10 15:17:47 +0000

    Files compiled!

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+export * from "./helpers";
+export * from "./types";
+export * as typography from "../dist/tokens/ts/typography";
+export * as lightThemeDocumentation from "../dist/documentation/themes/light";
+export * as darkThemeDocumentation from "../dist/documentation/themes/dark";
+export * as coreDarkDocumentation from "../dist/documentation/core-dark-colors";
+export * as coreLightDocumentation from "../dist/documentation/core-light-colors";
+//# sourceMappingURL=index.d.ts.map
