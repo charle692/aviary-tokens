06fb78b8bafe6008cf8155b3ca695483e7691cfc
alexandra-lim <alexandra-lim@users.noreply.github.com>
2023-02-10 15:53:18 +0000

    Transformed tokens

--- dist/types/themes.d.ts
+++
@@ -1,7 +0,0 @@
-import * as light from "../../dist/tokens/ts/themes/light";
-import * as dark from "../../dist/tokens/ts/themes/dark";
-import * as emerson from "../../dist/tokens/ts/themes/emerson";
-declare type AviaryTheme = typeof light;
-export { dark, light, emerson };
-export type { AviaryTheme };
-//# sourceMappingURL=themes.d.ts.map
