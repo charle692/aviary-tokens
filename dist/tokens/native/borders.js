bb287881d3178b5a2e0c64e21946cb1637598c44
alexandra-lim <alexandra-lim@users.noreply.github.com>
2023-02-16 15:05:07 +0000

    Built and updated design tokens

---
+++ dist/tokens/native/borders.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 16 Feb 2023 15:05:06 GMT
+ */
+
+module.exports = {
+  "base": 4,
+  "card": 8,
+  "circle": "50%",
+  "modal": 8,
+  "pill": 100
+};
---
+++ dist/tokens/ts/borders.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 16 Feb 2023 15:05:06 GMT
+ */
+
+module.exports = {
+  "base": "4px",
+  "card": "8px",
+  "circle": "50%",
+  "modal": "8px",
+  "pill": "100px"
+};
