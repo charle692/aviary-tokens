d506747b0b22abd30f180b4cda9d1a36073d4d70
alexandra-lim <alexandra-lim@users.noreply.github.com>
2023-02-22 15:42:29 +0000

    Built and updated design tokens

---
+++ dist/tokens/native/borders.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 22 Feb 2023 15:42:28 GMT
+ */
+
+export const base : number;
+export const card : number;
+export const circle : string;
+export const modal : number;
+export const pill : number;
---
+++ dist/tokens/ts/borders.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 22 Feb 2023 15:42:28 GMT
+ */
+
+export const base : string;
+export const card : string;
+export const circle : string;
+export const modal : string;
+export const pill : string;
