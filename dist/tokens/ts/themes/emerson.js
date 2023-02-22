92ec8ea361ff7ddc864cb1f142ad066f150f5616
alexandra-lim <alexandra-lim@users.noreply.github.com>
2023-02-22 22:40:13 +0000

    Built and updated design tokens

---
+++ dist/tokens/ts/themes/emerson.js
@@ -0,0 +1,18 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 22 Feb 2023 22:40:13 GMT
+ */
+
+module.exports = {theme: "emerson",
+text : {emphasis : "#1A1D21",body : "#22272B",subdued : "#2D3238",onBackground : "#FFFFFF"},
+system : {textLabelEmphasis : "#101012",textBase : "#22272B",textHover : "#1A1D21",textActive : "#101012",backgroundBase : "#FFFFFF",backgroundMuted : "#98999B",backgroundMutedHover : "#616568",backgroundMutedActive : "#515357",backgroundBox : "#E8E9EA",borderBase : "#515357",borderHover : "#2D3238",borderActive : "#22272B",borderFocused : "#0281d7"},
+surface : {level0 : "#98999B",level1 : "#FFFFFF",level2 : "#98999B",interactiveBackground : "#FFFFFF",interactiveHover : "#98999B",interactiveActive : "#616568",overlayDark : "#101012",overlayTheme : "#FFFFFF",overlayBackdrop : "#1A1D21"},
+primary : {textBase : "#307553",textHover : "#275E43",textActive : "#244C38",backgroundBase : "#307553",backgroundHover : "#275E43",backgroundActive : "#244C38",backgroundMuted : "#EBF2EF",backgroundMutedHover : "#D1E0D9",backgroundMutedActive : "#B6CFC2",backgroundBox : "#FAFFFC",borderBase : "#307553",borderHover : "#275E43",borderActive : "#244C38"},
+info : {textBase : "#0281d7",textHover : "#21588F",textActive : "#194673",backgroundBase : "#0281d7",backgroundHover : "#21588F",backgroundActive : "#194673",backgroundMuted : "#E6F1FC",backgroundMutedHover : "#C0D8F0",backgroundMutedActive : "#88B1D9",backgroundBox : "#F5FAFF",borderBase : "#0281d7",borderHover : "#21588F",borderActive : "#194673"},
+warning : {textBase : "#B4631D",textHover : "#8E4D14",textActive : "#784213",backgroundBase : "#B4631D",backgroundHover : "#8E4D14",backgroundActive : "#784213",backgroundMuted : "#FFF3E8",backgroundMutedHover : "#F3D8C0",backgroundMutedActive : "#E7B88F",backgroundBox : "#FFFAF5",borderBase : "#B4631D",borderHover : "#8E4D14",borderActive : "#784213"},
+danger : {textBase : "#AF2645",textHover : "#980B29",textActive : "#800D25",backgroundBase : "#AF2645",backgroundHover : "#980B29",backgroundActive : "#800D25",backgroundMuted : "#FEEEF2",backgroundMutedHover : "#F0C4CD",backgroundMutedActive : "#E296A6",backgroundBox : "#FFF7F9",borderBase : "#AF2645",borderHover : "#980B29",borderActive : "#800D25"},
+highlight : {textBase : "#533E7D",textHover : "#3B2566",textActive : "#2A174F",backgroundBase : "#533E7D",backgroundHover : "#3B2566",backgroundActive : "#2A174F",backgroundMuted : "#F2ECFE",backgroundMutedHover : "#D7CEE9",backgroundMutedActive : "#B9ABD5",backgroundBox : "#FCFAFF",borderBase : "#533E7D",borderHover : "#3B2566",borderActive : "#2A174F"},
+disabled : {text : "#2D3238",background : "#98999B",nakedBackground : "transparent",border : "#515357",inputBackground : "#616568",inputLabelText : "#2D3238"},
+input : {backgroundBase : "#98999B",backgroundHover : "#616568",backgroundInformation : "#FFFFFF",textBase : "#22272B",textInformation : "#22272B",textInformationError : "#AF2645",textLabel : "#22272B",textFloatingLabel : "#0281d7",textFloatingLabelError : "#AF2645",textPlaceholder : "#2D3238",textActive : "#101012",borderBase : "#616568",borderError : "#AF2645",borderHover : "#515357",borderEmphasized : "#2D3238",borderEmphasizedHover : "#22272B",borderActive : "#0281d7"},
+separator : {base : "#616568"},
+light : {backgroundBase : "#FFFFFF",backgroundContrast : "#22272B",backgroundHover : "#98999B",backgroundActive : "#616568",textBase : "#101012",linkMutedBase : "#98999B",linkMutedHover : "#FFFFFF"},};
