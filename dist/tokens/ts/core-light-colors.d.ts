676a10a08b07911e5ff6e39a498d4041feb4cf98
CCAyl <CCAyl@users.noreply.github.com>
2023-05-02 17:33:03 +0000

    Built and updated design tokens

---
+++ dist/tokens/ts/core-light-colors.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 02 May 2023 17:33:01 GMT
+ */
+
+export const green : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const blue : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const red : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const purple : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const orange : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const grey : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const shades : {white : string,black : string,transparent : string};
+export const yellow : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
