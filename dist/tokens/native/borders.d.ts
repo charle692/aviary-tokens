629163a7e7d2503eaf256c18e7f5c7604c6d7e03
alexandra-lim <alexandra-lim@users.noreply.github.com>
2023-05-12 16:40:48 +0000

    Built and updated design tokens

---
+++ dist/tokens/native/borders.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 12 May 2023 16:40:48 GMT
+ */
+
+export const radiusBase : number;
+export const radiusCircle : string;
+export const radiusModal : number;
+export const radiusPill : number;
+export const widthBase : number;
---
+++ dist/tokens/ts/borders.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 12 May 2023 16:40:48 GMT
+ */
+
+module.exports = {
+  "radiusBase": "8px",
+  "radiusCircle": "50%",
+  "radiusModal": "8px",
+  "radiusPill": "100px",
+  "widthBase": "1px"
+};
