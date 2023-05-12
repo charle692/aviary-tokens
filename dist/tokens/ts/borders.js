8f8688399c3bf6095ca5c6f5929c636f422115ce
alexandra-lim <alexandra-lim@users.noreply.github.com>
2023-05-12 14:39:11 +0000

    Built and updated design tokens

---
+++ dist/tokens/ts/borders.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 12 May 2023 14:39:10 GMT
+ */
+
+module.exports = {
+  "radiusBase": "8px",
+  "radiusCircle": "50%",
+  "radiusModal": "8px",
+  "radiusPill": "100px",
+  "widthBase": "1px"
+};
---
+++ dist/tokens/ts/themes/light.js
@@ -0,0 +1,19 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 12 May 2023 14:39:10 GMT
+ */
+
+module.exports = {theme: "light",
+text : {emphasis : "#36485C",body : "#475A70",subdued : "#596D84",onBackground : "#FFFFFF"},
+system : {textLabelEmphasis : "#2E3A47",textBase : "#475A70",textHover : "#36485C",textActive : "#2E3A47",backgroundBase : "#FFFFFF",backgroundMuted : "#F5F7FA",backgroundMutedHover : "#E6EDF5",backgroundMutedActive : "#C8D3E0",backgroundBox : "#FCFEFF",borderBase : "#C8D3E0",borderHover : "#596D84",borderActive : "#475A70",borderFocused : "#3971A8"},
+surface : {level0 : "#F5F7FA",level1 : "#FFFFFF",level2 : "#F5F7FA",interactiveBackground : "#FFFFFF",interactiveHover : "#F5F7FA",interactiveActive : "#E6EDF5",overlayDark : "#2E3A47",overlayTheme : "#FFFFFF",overlayBackdrop : "#36485C"},
+primary : {textBase : "#307553",textHover : "#275E43",textActive : "#244C38",backgroundBase : "#307553",backgroundHover : "#275E43",backgroundActive : "#244C38",backgroundMuted : "#EBF2EF",backgroundMutedHover : "#D1E0D9",backgroundMutedActive : "#B6CFC2",backgroundBox : "#FAFFFC",borderBase : "#307553",borderHover : "#275E43",borderActive : "#244C38"},
+info : {textBase : "#3971A8",textHover : "#21588F",textActive : "#194673",backgroundBase : "#3971A8",backgroundHover : "#21588F",backgroundActive : "#194673",backgroundMuted : "#E6F1FC",backgroundMutedHover : "#C0D8F0",backgroundMutedActive : "#88B1D9",backgroundBox : "#F5FAFF",borderBase : "#3971A8",borderHover : "#21588F",borderActive : "#194673"},
+warning : {textBase : "#B4631D",textHover : "#8E4D14",textActive : "#784213",backgroundBase : "#B4631D",backgroundHover : "#8E4D14",backgroundActive : "#784213",backgroundMuted : "#FFF3E8",backgroundMutedHover : "#F3D8C0",backgroundMutedActive : "#E7B88F",backgroundBox : "#FFFAF5",borderBase : "#B4631D",borderHover : "#8E4D14",borderActive : "#784213"},
+danger : {textBase : "#AF2645",textHover : "#980B29",textActive : "#800D25",backgroundBase : "#AF2645",backgroundHover : "#980B29",backgroundActive : "#800D25",backgroundMuted : "#FEEEF2",backgroundMutedHover : "#F0C4CD",backgroundMutedActive : "#E296A6",backgroundBox : "#FFF7F9",borderBase : "#AF2645",borderHover : "#980B29",borderActive : "#800D25"},
+highlight : {textBase : "#533E7D",textHover : "#3B2566",textActive : "#2A174F",backgroundBase : "#533E7D",backgroundHover : "#3B2566",backgroundActive : "#2A174F",backgroundMuted : "#F2ECFE",backgroundMutedHover : "#D7CEE9",backgroundMutedActive : "#B9ABD5",backgroundBox : "#FCFAFF",borderBase : "#533E7D",borderHover : "#3B2566",borderActive : "#2A174F"},
+disabled : {text : "#596D84",background : "#F5F7FA",nakedBackground : "transparent",border : "#C8D3E0",inputBackground : "#E6EDF5",inputLabelText : "#596D84"},
+input : {backgroundBase : "#F5F7FA",backgroundHover : "#E6EDF5",backgroundInformation : "#FFFFFF",textBase : "#475A70",textInformation : "#475A70",textInformationError : "#AF2645",textLabel : "#475A70",textFloatingLabel : "#3971A8",textFloatingLabelError : "#AF2645",textPlaceholder : "#596D84",textActive : "#2E3A47",borderBase : "#E6EDF5",borderError : "#AF2645",borderHover : "#C8D3E0",borderEmphasized : "#596D84",borderEmphasizedHover : "#475A70",borderActive : "#3971A8"},
+separator : {base : "#E6EDF5"},
+light : {backgroundBase : "#FFFFFF",backgroundContrast : "#475A70",backgroundHover : "#F5F7FA",backgroundActive : "#E6EDF5",textBase : "#2E3A47",linkMutedBase : "#F5F7FA",linkMutedHover : "#FFFFFF"},
+shadow : {base : "#36485C"},boxShadows: {cardBase : "0px 1px 2px 0px #36485c29",cardHover : "0px 2px 8px 0px #36485c33",popoverBase : "0px 4px 16px 0px #36485c29",modalBase : "0px 8px 32px 0px #36485c3d"},};
