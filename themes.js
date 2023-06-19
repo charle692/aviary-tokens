a3cc6c95e59b3bce781dca1868e49d685afaed2c
Ian Clarke <me@iryanclarke.com>
2023-06-19 16:15:37 -0400

    fix: import Fixes (#113)
    Co-authored-by: Ian Clarke <ian.clarke@fullscript.com>

--- themes.js
+++ themes.js
@@ -43,24 +43,22 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.lightDS3 = exports.light = exports.emerson = exports.dark = void 0;
 
-var light = _interopRequireWildcard(
-  require("../../dist/tokens/ts/themes/light")
-);
+var light = _interopRequireWildcard(require("./dist/tokens/ts/themes/light"));
 
 exports.light = light;
 
 var lightDS3 = _interopRequireWildcard(
-  require("../../dist/tokens/ts/themes/lightDS3")
+  require("./dist/tokens/ts/themes/lightDS3")
 );
 
 exports.lightDS3 = lightDS3;
 
-var dark = _interopRequireWildcard(require("../../dist/tokens/ts/themes/dark"));
+var dark = _interopRequireWildcard(require("./dist/tokens/ts/themes/dark"));
 
 exports.dark = dark;
 
 var emerson = _interopRequireWildcard(
-  require("../../dist/tokens/ts/themes/emerson")
+  require("./dist/tokens/ts/themes/emerson")
 );
 
 exports.emerson = emerson;
