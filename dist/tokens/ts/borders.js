b2e74b3585ace5f2cbaede6b47615373142b504b
CCAyl <CCAyl@users.noreply.github.com>
2023-07-11 17:31:37 +0000

    Built and updated design tokens

---
+++ dist/tokens/ts/borders.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 11 Jul 2023 17:31:36 GMT
+ */
+
+module.exports = {
+  "radiusBase": "8px",
+  "radiusCircle": "50%",
+  "radiusModal": "8px",
+  "radiusPill": "100px",
+  "widthBase": "1px"
+};
