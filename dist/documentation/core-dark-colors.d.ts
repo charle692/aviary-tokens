9a40bc85ce927c1eead0097ebed8de69ebb78e1c
CCAyl <CCAyl@users.noreply.github.com>
2023-07-11 13:36:43 +0000

    Built and updated design tokens

---
+++ dist/documentation/core-dark-colors.d.ts
@@ -0,0 +1,216 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 11 Jul 2023 13:36:42 GMT
+ */
+
+export const green : {100:{
+            name: string,
+            hex: string,
+            
+          },200:{
+            name: string,
+            hex: string,
+            
+          },300:{
+            name: string,
+            hex: string,
+            
+          },400:{
+            name: string,
+            hex: string,
+            
+          },500:{
+            name: string,
+            hex: string,
+            
+          },600:{
+            name: string,
+            hex: string,
+            
+          },700:{
+            name: string,
+            hex: string,
+            
+          },800:{
+            name: string,
+            hex: string,
+            
+          }};
+export const blue : {100:{
+            name: string,
+            hex: string,
+            
+          },200:{
+            name: string,
+            hex: string,
+            
+          },300:{
+            name: string,
+            hex: string,
+            
+          },400:{
+            name: string,
+            hex: string,
+            
+          },500:{
+            name: string,
+            hex: string,
+            
+          },600:{
+            name: string,
+            hex: string,
+            
+          },700:{
+            name: string,
+            hex: string,
+            
+          },800:{
+            name: string,
+            hex: string,
+            
+          }};
+export const red : {100:{
+            name: string,
+            hex: string,
+            
+          },200:{
+            name: string,
+            hex: string,
+            
+          },300:{
+            name: string,
+            hex: string,
+            
+          },400:{
+            name: string,
+            hex: string,
+            
+          },500:{
+            name: string,
+            hex: string,
+            
+          },600:{
+            name: string,
+            hex: string,
+            
+          },700:{
+            name: string,
+            hex: string,
+            
+          },800:{
+            name: string,
+            hex: string,
+            
+          }};
+export const purple : {100:{
+            name: string,
+            hex: string,
+            
+          },200:{
+            name: string,
+            hex: string,
+            
+          },300:{
+            name: string,
+            hex: string,
+            
+          },400:{
+            name: string,
+            hex: string,
+            
+          },500:{
+            name: string,
+            hex: string,
+            
+          },600:{
+            name: string,
+            hex: string,
+            
+          },700:{
+            name: string,
+            hex: string,
+            
+          },800:{
+            name: string,
+            hex: string,
+            
+          }};
+export const orange : {100:{
+            name: string,
+            hex: string,
+            
+          },200:{
+            name: string,
+            hex: string,
+            
+          },300:{
+            name: string,
+            hex: string,
+            
+          },400:{
+            name: string,
+            hex: string,
+            
+          },500:{
+            name: string,
+            hex: string,
+            
+          },600:{
+            name: string,
+            hex: string,
+            
+          },700:{
+            name: string,
+            hex: string,
+            
+          },800:{
+            name: string,
+            hex: string,
+            
+          }};
+export const grey : {100:{
+            name: string,
+            hex: string,
+            
+          },200:{
+            name: string,
+            hex: string,
+            
+          },300:{
+            name: string,
+            hex: string,
+            
+          },400:{
+            name: string,
+            hex: string,
+            
+          },500:{
+            name: string,
+            hex: string,
+            
+          },600:{
+            name: string,
+            hex: string,
+            
+          },700:{
+            name: string,
+            hex: string,
+            
+          },800:{
+            name: string,
+            hex: string,
+            
+          }};
+export const shades : {white:{
+            name: string,
+            hex: string,
+            
+          },black:{
+            name: string,
+            hex: string,
+            
+          },transparent:{
+            name: string,
+            hex: string,
+            
+          }};
---
+++ dist/tokens/native/borders.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 11 Jul 2023 13:36:42 GMT
+ */
+
+module.exports = {
+  "radiusBase": 8,
+  "radiusCircle": "50%",
+  "radiusModal": 8,
+  "radiusPill": 100,
+  "widthBase": 1
+};
