f31162a0dc564183e745721d1a3d36e1c4219e9f
CCAyl <CCAyl@users.noreply.github.com>
2023-07-12 18:23:56 +0000

    Files compiled!

---
+++ dist/types/interfaces.d.ts
@@ -0,0 +1,28 @@
+declare const AVIARY_COLORS: {
+    primary: string;
+    info: string;
+    warning: string;
+    danger: string;
+    highlight: string;
+    system: string;
+};
+declare const EXTENDED_AVIARY_COLORS: {
+    light: string;
+    primary: string;
+    info: string;
+    warning: string;
+    danger: string;
+    highlight: string;
+    system: string;
+};
+declare type AviaryColors = keyof typeof AVIARY_COLORS;
+declare type ExtendedAviaryColors = keyof typeof EXTENDED_AVIARY_COLORS;
+interface AviaryColorProps {
+    isColor?: AviaryColors;
+}
+interface AviaryExtendedColorProps {
+    isColor?: ExtendedAviaryColors;
+}
+export type { AviaryColors, AviaryColorProps, AviaryExtendedColorProps, ExtendedAviaryColors };
+export { AVIARY_COLORS, EXTENDED_AVIARY_COLORS };
+//# sourceMappingURL=interfaces.d.ts.map
