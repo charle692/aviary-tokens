c5942f35c3c9161209cacbf2e1c97d3f9c649015
alexandra-lim <alexandra-lim@users.noreply.github.com>
2023-09-12 15:33:17 +0000

    Built and updated design tokens

---
+++ dist/tokens/ts/borders.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 12 Sep 2023 15:33:16 GMT
+ */
+
+module.exports = {
+  "radiusBase": "8px",
+  "radiusCircle": "50%",
+  "radiusModal": "8px",
+  "radiusPill": "100px",
+  "widthBase": "1px"
+};
