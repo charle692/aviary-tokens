d7139787e48424d925d5de153185be8c46a24d8e
alexandra-lim <alexandra-lim@users.noreply.github.com>
2023-09-19 21:06:54 +0000

    Built and updated design tokens

---
+++ dist/tokens/native/core-light-colors.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 19 Sep 2023 21:06:53 GMT
+ */
+
+export const green : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const blue : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const red : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const purple : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const orange : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const grey : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string,900 : string};
+export const shades : {white : string,black : string,transparent : string};
+export const yellow : {100 : string,200 : string,300 : string,400 : string,500 : string,600 : string,700 : string,800 : string};
+export const brown : {200 : string,300 : string,400 : string,700 : string};
---
+++ dist/tokens/ts/borders.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 19 Sep 2023 21:06:53 GMT
+ */
+
+export const radiusBase : string;
+export const radiusCircle : string;
+export const radiusModal : string;
+export const radiusPill : string;
+export const widthBase : string;
