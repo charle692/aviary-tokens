59493427eb4bee82ee1f6a4cb47484fd611ec6c5
bambie1 <bambie1@users.noreply.github.com>
2023-09-22 12:47:37 +0000

    Built and updated design tokens

---
+++ dist/tokens/native/borders.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 22 Sep 2023 12:47:36 GMT
+ */
+
+export const radiusBase : number;
+export const radiusCircle : string;
+export const radiusModal : number;
+export const radiusPill : number;
+export const widthBase : number;
