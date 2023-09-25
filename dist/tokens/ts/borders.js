74550e636b2f6d6ac33c96bd4663797f16923977
bambie1 <bambie1@users.noreply.github.com>
2023-09-25 14:33:27 +0000

    Built and updated design tokens

---
+++ dist/tokens/ts/borders.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 25 Sep 2023 14:33:26 GMT
+ */
+
+module.exports = {
+  "radiusBase": "8px",
+  "radiusCircle": "50%",
+  "radiusModal": "8px",
+  "radiusPill": "100px",
+  "widthBase": "1px"
+};
