1079ef32073709cc68dfc7d3efcc5cbd4c44b87e
AnnaAres <AnnaAres@users.noreply.github.com>
2023-09-28 17:40:36 +0000

    Built and updated design tokens

---
+++ dist/tokens/native/borders.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 28 Sep 2023 17:40:35 GMT
+ */
+
+module.exports = {
+  "radiusBase": 8,
+  "radiusCircle": "50%",
+  "radiusModal": 8,
+  "radiusPill": 100,
+  "widthBase": 1
+};
