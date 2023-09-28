e253107f04eed5e6c57b25a55f8a69cb22391c99
AnnaAres <AnnaAres@users.noreply.github.com>
2023-09-28 17:47:38 +0000

    Built and updated design tokens

---
+++ dist/tokens/ts/borders.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 28 Sep 2023 17:47:37 GMT
+ */
+
+export const radiusBase : string;
+export const radiusCircle : string;
+export const radiusModal : string;
+export const radiusPill : string;
+export const widthBase : string;
