fd34d7ac81a046ba2daeee6726852a8b754fa863
CCAyl <CCAyl@users.noreply.github.com>
2023-10-16 21:14:55 +0000

    Built and updated design tokens

---
+++ dist/tokens/native/borders.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 16 Oct 2023 21:14:54 GMT
+ */
+
+export const radiusBase : number;
+export const radiusCircle : string;
+export const radiusModal : number;
+export const radiusPill : number;
+export const widthBase : number;
