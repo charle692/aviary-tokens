d372a7bc0b52e099545b99731d293447c5a3cac7
alexandra-lim <alexandra-lim@users.noreply.github.com>
2023-10-18 16:23:46 +0000

    Files compiled!

---
+++ dist/types/intentions.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.AVIARY_INTENTIONS = void 0;
+var AVIARY_INTENTIONS = {
+  primary: "primary",
+  system: "system",
+  danger: "danger",
+  success: "success",
+  textSuccess: "textSuccess",
+  textSystem: "textSystem",
+  textDanger: "textDanger",
+  lightFilled: "lightFilled",
+  lightOutlined: "lightOutlined",
+  lightText: "lightText"
+};
+exports.AVIARY_INTENTIONS = AVIARY_INTENTIONS;
