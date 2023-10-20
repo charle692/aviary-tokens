a09724e98974289327ff0851f90e019fcd40614f
alexandra-lim <alexandra-lim@users.noreply.github.com>
2023-10-20 13:14:25 +0000

    Built and updated design tokens

---
+++ dist/tokens/native/borders.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 20 Oct 2023 13:14:24 GMT
+ */
+
+export const radiusBase : number;
+export const radiusCircle : string;
+export const radiusModal : number;
+export const radiusPill : number;
+export const widthBase : number;
---
+++ dist/tokens/ts/borders.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 20 Oct 2023 13:14:24 GMT
+ */
+
+export const radiusBase : string;
+export const radiusCircle : string;
+export const radiusModal : string;
+export const radiusPill : string;
+export const widthBase : string;
