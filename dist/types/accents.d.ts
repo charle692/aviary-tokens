e03d7b10fa94320dcdf2e54af502a8f1fb6663a4
alexandra-lim <alexandra-lim@users.noreply.github.com>
2023-10-20 13:13:50 +0000

    Transformed tokens

--- dist/types/accents.d.ts
+++
@@ -1,8 +0,0 @@
-declare const AVIARY_ACCENTS: {
-    forest: string;
-    sand: string;
-};
-declare type AviaryAccents = keyof typeof AVIARY_ACCENTS;
-export type { AviaryAccents };
-export { AVIARY_ACCENTS };
-//# sourceMappingURL=accents.d.ts.map
