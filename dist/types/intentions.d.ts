b620db68f8cdd6557698b3de1744f8cc9c295ebf
CCAyl <CCAyl@users.noreply.github.com>
2023-11-21 18:29:38 +0000

    Files compiled!

---
+++ dist/types/intentions.d.ts
@@ -0,0 +1,16 @@
+declare const AVIARY_INTENTIONS: {
+    primary: string;
+    system: string;
+    danger: string;
+    success: string;
+    textSuccess: string;
+    textSystem: string;
+    textDanger: string;
+    lightFilled: string;
+    lightOutlined: string;
+    lightText: string;
+};
+declare type AviaryIntentions = keyof typeof AVIARY_INTENTIONS;
+export type { AviaryIntentions };
+export { AVIARY_INTENTIONS };
+//# sourceMappingURL=intentions.d.ts.map
