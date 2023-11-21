76265c6033f5410c6fd9934f02403ff3371925d9
Benaiah Barango <bennieb96@gmail.com>
2023-11-21 08:16:26 -0500

    change OR to AND

--- src/types/profiles.ts
+++ src/types/profiles.ts
@@ -3,6 +3,6 @@ import type * as light from "../../dist/tokens/ts/themes/light.d";
 type StandardColorsProfileTheme = typeof light.primary;
 type SystemColorProfileTheme = typeof light.system;
 
-type ColorProfileTheme = StandardColorsProfileTheme | SystemColorProfileTheme;
+type ColorProfileTheme = StandardColorsProfileTheme & SystemColorProfileTheme;
 
 export type { ColorProfileTheme };
