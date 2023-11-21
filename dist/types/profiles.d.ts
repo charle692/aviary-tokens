3e790ef46754ba5171cf4550d8f9ab19692087b1
bambie1 <bambie1@users.noreply.github.com>
2023-11-21 13:17:52 +0000

    Files compiled!

---
+++ dist/types/profiles.d.ts
@@ -0,0 +1,6 @@
+import type * as light from "../../dist/tokens/ts/themes/light.d";
+declare type StandardColorsProfileTheme = typeof light.primary;
+declare type SystemColorProfileTheme = typeof light.system;
+declare type ColorProfileTheme = StandardColorsProfileTheme & SystemColorProfileTheme;
+export type { ColorProfileTheme };
+//# sourceMappingURL=profiles.d.ts.map
