29fecd28dadf8a90306f414c30bb62257441bfde
CCAyl <CCAyl@users.noreply.github.com>
2023-11-22 14:30:47 +0000

    Transformed tokens

--- dist/tokens/ts/dimensions.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Tue, 21 Nov 2023 19:04:19 GMT
- */
-
-export const contentAreaMax : string;
--- dist/tokens/ts/dimensions.js
+++
@@ -1,8 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Tue, 21 Nov 2023 19:04:19 GMT
- */
-
-module.exports = {
-  "contentAreaMax": "1196px"
-};
