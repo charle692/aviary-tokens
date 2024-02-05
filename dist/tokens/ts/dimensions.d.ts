5a5be5b4dc270516c3bd26ab695cd5faa811eb22
bambie1 <bambie1@users.noreply.github.com>
2024-02-05 19:15:59 +0000

    Transformed tokens

--- dist/tokens/ts/dimensions.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Fri, 02 Feb 2024 20:01:56 GMT
- */
-
-export const contentAreaMax : string;
--- dist/tokens/ts/dimensions.js
+++
@@ -1,8 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Fri, 02 Feb 2024 20:01:56 GMT
- */
-
-module.exports = {
-  "contentAreaMax": "1196px"
-};
